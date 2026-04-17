e7bbb963bb5a18349697ff766cb4fa92dfdaa31a
imrichardwu <richard9@ualberta.ca>
2026-04-16 18:29:15 -0700

    feat: add client networking protocol

--- client/src/games/mod.rs
+++ client/src/games/mod.rs
@@ -1,3 +1,7 @@
 pub mod five_card_draw;
 pub mod seven_card_stud;
 pub mod texas_holdem;
+
+pub use five_card_draw::five_card_draw;
+pub use seven_card_stud::seven_card_stud;
+pub use texas_holdem::texas_holdem;
