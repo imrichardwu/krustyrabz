a7ced6bc63d564a01c818439a807175d7193e950
imrichardwu <richard9@ualberta.ca>
2026-04-17 18:07:29 -0700

    feat: track player statistics

---
+++ core/src/house.rs
@@ -0,0 +1,38 @@
+use std::sync::{Arc, Mutex}; 
+use threadpool::ThreadPool; 
+//TODO need something like use std::
+use std::thread; 
+
+//dealer-threads let players buy into games 
+const n_floorstaff: usize = 20;
+
+pub mod game;
+pub use game::{Game}; 
+
+pub struct House { 
+    pub live_games: vec<Game>;
+    pub floorstaff_pool: ThreadPool //in casinos, floorstaff help players find open tables
+}
+
+impl House {
+    pub fn new() -> Self {
+        let mut live_games = vec![]; 
+        let floorstaff_pool = ThreadPool::new(n_floorstaff); 
+    }
+
+    //get connections, add player 
+    //whenever a player enters, a dealer/thread 
+    //is tasked with admitting that player to a game
+    //ThreadPool, Arc with mutex 
+    pub fn open_doors() {
+        while true {
+         ;   
+        }
+    }
+
+    pub fn place_new_player_at_open_table(&mut self) {
+        ; 
+    }
+
+    //pit boss: opens new games  
+}
---
+++ storage/src/repository.rs
@@ -0,0 +1,5 @@
+use supabase_rs::SupabaseClient; 
+use serde_json::json;
+use dotenv::dotenv; 
+
+//crates.io/crates/supabase  see boilerplate there
